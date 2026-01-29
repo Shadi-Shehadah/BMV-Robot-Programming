db975a1531bc2b43b0725e60aae2cec3001fb455
Leon Okello <okellopatrickleon@gmail.com>
2026-01-29 10:18:51 +0100

    Added the test code wit motor stuff

--- main.py
+++ main.py
@@ -13,8 +13,27 @@ from pybricks.media.ev3dev import SoundFile, ImageFile
 
 
 # Create your objects here.
+# ev3 = EV3Brick()
+
+
+# # Write your program here.
+# ev3.speaker.beep()
+
+# Create your objects here
+
+# Initialize the EV3 Brick.
 ev3 = EV3Brick()
 
+# Initialize a motor at port B.
+test_motor = Motor(Port.B)
 
-# Write your program here.
+# Write your program here
+
+# Play a sound.
 ev3.speaker.beep()
+
+# Run the motor up to 500 degrees per second. To a target angle of 90 degrees.
+test_motor.run_target(500, 90)
+
+# Play another beep sound.
+ev3.speaker.beep(frequency=1000, duration=500)
